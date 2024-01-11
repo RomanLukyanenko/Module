5c33e2b7e4ee1cc460b72d5a681646b447082fed
Roman <roman.lukyanenko.rl@gmail.com>
2024-01-11 06:42:45 +0200

    Ready for deploy

--- src/components/search/LoadingIndicator.jsx
+++ src/components/search/LoadingIndicator.jsx
@@ -2,7 +2,7 @@ export const LoadingIndicator = () => {
   return (
     // Контейнер для індикатора завантаження
     <div className="catalog__content" id="catalog-products">
-      // Текст, який відображається під час завантаження
+      {/* // Текст, який відображається під час завантаження */}
       <p>Завантаження...</p>
     </div>
   );

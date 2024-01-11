a757cb53df98d58370653c317a4a77dffccfcbac
Roman <roman.lukyanenko.rl@gmail.com>
2024-01-11 21:11:43 +0200

    Add useNavigate

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,31 +1,54 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate  } from "react-router-dom";
 
 export const Footer = () => {
+  const navigate = useNavigate();
+
+  const handleLinkClick = (path) => {
+    navigate(path);
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
   return (
     <div className="footer">
       <div className="container">
-        <Link to="/" className="logo">
+        <Link to="/" className="logo" onClick={() => handleLinkClick("/")}>
           I-happy
         </Link>
 
         <ul className="navbar-nav menu">
           <li className="nav-item menu__li">
-            <Link to="/" className="nav-link menu__link link-hover">
+            <Link
+              to="/"
+              className="nav-link menu__link link-hover"
+              onClick={() => handleLinkClick("/")}
+            >
               Каталог
             </Link>
           </li>
           <li className="nav-item menu__li">
-            <Link to="/about-us" className="nav-link menu__link link-hover">
+            <Link
+              to="/about-us"
+              className="nav-link menu__link link-hover"
+              onClick={() => handleLinkClick("/about-us")}
+            >
               Про нас
             </Link>
           </li>
           <li className="nav-item menu__li">
-            <Link to="/delivery" className="nav-link menu__link link-hover">
+            <Link
+              to="/delivery"
+              className="nav-link menu__link link-hover"
+              onClick={() => handleLinkClick("/delivery")}
+            >
               Доставка і оплата
             </Link>
           </li>
           <li className="nav-item menu__li">
-            <Link to="/contacts" className="nav-link menu__link link-hover">
+            <Link
+              to="/contacts"
+              className="nav-link menu__link link-hover"
+              onClick={() => handleLinkClick("/contacts")}
+            >
               Контакти
             </Link>
           </li>

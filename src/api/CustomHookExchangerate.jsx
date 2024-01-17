e919f2af82db69a7a9373261da45a0816b6422f4
Roman <roman.lukyanenko.rl@gmail.com>
2024-01-17 04:17:27 +0200

    Added time for API

--- src/api/CustomHookExchangerate.jsx
+++ src/api/CustomHookExchangerate.jsx
@@ -2,15 +2,33 @@ import { useEffect, useState } from "react";
 
 export const useCurrencyRates = () => {
     const [currencyData, setCurrencyData] = useState({ EUR: 1, USD: 1 });
-  
+
     useEffect(() => {
-      fetch("https://v6.exchangerate-api.com/v6/49107cb6b4bd5d052a47345c/latest/UAH")
-        .then(response => response.json())
-        .then(data => {
-          const { conversion_rates } = data;
-          setCurrencyData({ EUR: conversion_rates.EUR, USD: conversion_rates.USD });
-        });
+        const data = localStorage.getItem('currencyRates');
+        const lastFetchTime = localStorage.getItem('lastFetchTime');
+
+        const now = new Date();
+        const lastFetchDate = new Date(lastFetchTime);
+        const nextUpdateTime = new Date(lastFetchDate);
+        nextUpdateTime.setUTCHours(22, 0, 0, 0); 
+        nextUpdateTime.setDate(lastFetchDate.getUTCDate() + 1);
+
+        const shouldUpdate = !data || now >= nextUpdateTime;
+
+        if (shouldUpdate) {
+            fetch("https://v6.exchangerate-api.com/v6/49107cb6b4bd5d052a47345c/latest/UAH")
+                .then(response => response.json())
+                .then(data => {
+                    const { conversion_rates } = data;
+                    const rates = { EUR: conversion_rates.EUR, USD: conversion_rates.USD };
+                    setCurrencyData(rates);
+                    localStorage.setItem('currencyRates', JSON.stringify(rates));
+                    localStorage.setItem('lastFetchTime', now.toISOString());
+                });
+        } else if (data) {
+            setCurrencyData(JSON.parse(data));
+        }
     }, []);
   
     return currencyData;
-  };
+};

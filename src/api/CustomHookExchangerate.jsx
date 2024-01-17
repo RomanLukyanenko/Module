f663ec3470e82b3b8c92f326bd8bc46ea6796bb1
Roman <roman.lukyanenko.rl@gmail.com>
2024-01-17 03:50:29 +0200

    Added new API for exchange

--- src/api/CustomHookExchangerate.jsx
+++ src/api/CustomHookExchangerate.jsx
@@ -4,7 +4,7 @@ export const useCurrencyRates = () => {
     const [currencyData, setCurrencyData] = useState({ EUR: 1, USD: 1 });
   
     useEffect(() => {
-      fetch("https://v6.exchangerate-api.com/v6/9703652e9e7fecda921aeacf/latest/UAH")
+      fetch("https://v6.exchangerate-api.com/v6/49107cb6b4bd5d052a47345c/latest/UAH")
         .then(response => response.json())
         .then(data => {
           const { conversion_rates } = data;

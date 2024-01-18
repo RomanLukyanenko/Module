3369dcb1348dbb9566cc8c9f303cd450f73e34a0
Roman <roman.lukyanenko.rl@gmail.com>
2024-01-18 05:29:56 +0200

    Added time to API exchange

--- src/section/cart/Cart.jsx
+++ src/section/cart/Cart.jsx
@@ -1,46 +1,15 @@
 import { useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  removeFromCart,
-  increaseItemCount,
-  decreaseItemCount,
-  changeCartVisibility,
-} from "../../store/CartSlice"; // Імпорт функцій Redux для управління станом кошика
+import { changeCartVisibility } from "../../store/CartSlice"; // Імпорт функцій Redux для управління станом кошика
 import { CartNoResult } from "./CartNoResult"; // Компонент для відображення повідомлення про порожній кошик
 import { CartItem } from "./CartItem"; // Компонент для відображення товару у кошику
 import { CartButton } from "./CartButton"; // Кнопка кошика
-import config from "../../store/config"; // Імпорт конфігурації (перевірте правильність шляху)
-
 export const Cart = () => {
   const dispatch = useDispatch(); // Використання хука для диспетчеризації дій Redux
 
   // Отримання даних кошика з Redux store
   const { cart, cartVisibility } = useSelector((state) => state.cart);
   const cartCount = cart.length; // Визначення кількості товарів у кошику
-
-  // Видалення товару з кошика
-  const delCartProduct = useCallback(
-    (productId) => {
-      dispatch(removeFromCart({ id: productId }));
-    },
-    [dispatch],
-  );
-
-  // Збільшення кількості товару в кошику
-  const setItemCountPlus = useCallback(
-    (productId) => {
-      dispatch(increaseItemCount({ id: productId }));
-    },
-    [dispatch],
-  );
-
-  // Зменшення кількості товару в кошику
-  const setItemCountMinus = useCallback(
-    (productId) => {
-      dispatch(decreaseItemCount({ id: productId }));
-    },
-    [dispatch],
-  );
 
   // Перемикання видимості кошика
   const toggleCart = useCallback(() => {
@@ -59,16 +28,7 @@ export const Cart = () => {
           <CartNoResult />
         ) : (
           // Відображення списку товарів у кошику
-          cart.map((product) => (
-            <CartItem
-              key={product.id}
-              product={product}
-              delCartProduct={delCartProduct}
-              setItemCountPlus={setItemCountPlus}
-              setItemCountMinus={setItemCountMinus}
-              pathImg={config.pathImg}
-            />
-          ))
+          cart.map((product) => <CartItem key={product.id} product={product} />)
         )}
       </div>
     </div>

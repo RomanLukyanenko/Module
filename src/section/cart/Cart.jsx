eb886f6c9ed13287e40758157e9ed1ce976e5377
Roman Lukianenko <142048423+RomanLukyanenko@users.noreply.github.com>
2024-06-03 23:29:51 +0300

    SomeChanges

--- src/section/cart/Cart.jsx
+++ src/section/cart/Cart.jsx
@@ -1,26 +1,20 @@
 import { useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
-<<<<<<< Updated upstream
-import { changeCartVisibility } from "../../store/CartSlice"; // Імпорт функцій Redux для управління станом кошика
-import { CartNoResult } from "./CartNoResult"; // Компонент для відображення повідомлення про порожній кошик
-import { CartItem } from "./CartItem"; // Компонент для відображення товару у кошику
-import { CartButton } from "./CartButton"; // Кнопка кошика
-=======
+import { changeCartVisibility } from "../../store/CartSlice";
+import { CartNoResult } from "./CartNoResult"; 
+import { CartItem } from "./CartItem"; 
+import { CartButton } from "./CartButton"; 
+
 import { changeCartVisibility } from "../../store/CartSlice";
 import { CartNoResult } from "./CartNoResult";
 import { CartItem } from "./CartItem";
 import { CartButton } from "./CartButton";
->>>>>>> Stashed changes
 export const Cart = () => {
   const dispatch = useDispatch();
 
   const { cart, cartVisibility } = useSelector((state) => state.cart);
   const cartCount = cart.length;
 
-<<<<<<< Updated upstream
-  // Перемикання видимості кошика
-=======
->>>>>>> Stashed changes
   const toggleCart = useCallback(() => {
     dispatch(changeCartVisibility());
   }, [dispatch]);
@@ -34,10 +28,6 @@ export const Cart = () => {
         {cart.length === 0 ? (
           <CartNoResult />
         ) : (
-<<<<<<< Updated upstream
-          // Відображення списку товарів у кошику
-=======
->>>>>>> Stashed changes
           cart.map((product) => <CartItem key={product.id} product={product} />)
         )}
       </div>
